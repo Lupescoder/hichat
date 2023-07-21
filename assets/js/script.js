1e54695daff1158c8a49a653ac074885f29f970e
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2023-07-21 15:07:28 -0300

    Add new mensage

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let inputMensage = document.querySelector('#newMensage');
+let containerNewMensage = document.querySelector('.chat-window__give');
+let chatWindow = document.querySelector('.chat__window');
+
+function checarEnter(event) {
+    if (event.key === "Enter") {
+        newMensage();
+    }
+}
+
+function newMensage () {
+    //Clona o balão de mensagem
+    let mensage = containerNewMensage.cloneNode(true);
+   
+    var dataAtual = new Date();
+    var horaAtual = dataAtual.toLocaleTimeString();
+
+    if(inputMensage.value != '') {
+        mensage.querySelector('.chat-window__give-msg').innerHTML = '';
+        mensage.querySelector('.chat-window__give-msg').innerHTML = inputMensage.value;
+
+        mensage.querySelector('.chat-window__give-time').innerHTML = '';
+        mensage.querySelector('.chat-window__give-time').innerHTML = horaAtual;
+
+        chatWindow.appendChild(mensage);
+        inputMensage.value = '';
+
+    }
+}
