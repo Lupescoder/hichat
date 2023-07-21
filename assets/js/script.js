6ceb4e650e1b4809c35156666580af150aa6b1c3
Lupescoder <wesleylupes@gmail.com>
2023-07-21 17:54:05 -0300

    Adicionando Socket no FrontEnds

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,30 @@ let inputMensage = document.querySelector('#newMensage');
 let containerNewMensage = document.querySelector('.chat-window__give');
 let chatWindow = document.querySelector('.chat__window');
 
+const socket = new WebSocket("ws://websocket-java-production.up.railway.app/ws");
+
+socket.onmessage = function (event) {
+    let mensage = containerNewMensage.cloneNode(true);
+   
+    var dataAtual = new Date();
+    var horaAtual = dataAtual.toLocaleTimeString();
+    
+    mensage.querySelector('.chat-window__give-msg').innerHTML = '';
+    mensage.querySelector('.chat-window__give-msg').innerHTML = event.data;
+
+    mensage.querySelector('.chat-window__give-time').innerHTML = '';
+    mensage.querySelector('.chat-window__give-time').innerHTML = horaAtual;
+
+    chatWindow.appendChild(mensage);
+
+    let newMensageSound = document.querySelector('.newMensage__sound');
+    newMensageSound.play();
+
+    inputMensage.value = '';
+
+}
+
+
 function checarEnter(event) {
     if (event.key === "Enter") {
         newMensage();
@@ -9,24 +33,7 @@ function checarEnter(event) {
 }
 
 function newMensage () {
-    let mensage = containerNewMensage.cloneNode(true);
-   
-    var dataAtual = new Date();
-    var horaAtual = dataAtual.toLocaleTimeString();
-
     if(inputMensage.value != '') {
-        mensage.querySelector('.chat-window__give-msg').innerHTML = '';
-        mensage.querySelector('.chat-window__give-msg').innerHTML = inputMensage.value;
-
-        mensage.querySelector('.chat-window__give-time').innerHTML = '';
-        mensage.querySelector('.chat-window__give-time').innerHTML = horaAtual;
-
-        chatWindow.appendChild(mensage);
-
-        let newMensageSound = document.querySelector('.newMensage__sound');
-        newMensageSound.play();
-
-        inputMensage.value = '';
-
+        socket.send(inputMensage.value);
     }
 }
