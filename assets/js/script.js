0acd20f9a9a621e5928fb970f13b935328cf12f2
Lupescoder <wesleylupes@gmail.com>
2023-07-22 17:18:35 -0300

    Adicionando o link do sevidor na cloud

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ let containerNewMensageGive = document.querySelector('.chat-window__give');
 let containerNewMensageRecive = document.querySelector('.chat-window__recive');
 let chatWindow = document.querySelector('.chat__window');
 
-const socket = new WebSocket("ws://localhost:8080/ws");
-
+const socket = new WebSocket("ws://websocket-java-production.up.railway.app/ws");
 
 socket.onmessage = function (event) {
     let mensageGive = containerNewMensageGive.cloneNode(true);
