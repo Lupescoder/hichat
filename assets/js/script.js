f6b03fe7a53ba8958c343cf71f3b62a5b7fa15e9
Lupescoder <wesleylupes@gmail.com>
2023-07-22 17:17:06 -0300

    Acresentando a comnicação entre varias sessões com o backend

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,46 @@
 let inputMensage = document.querySelector('#newMensage');
-let containerNewMensage = document.querySelector('.chat-window__give');
+let containerNewMensageGive = document.querySelector('.chat-window__give');
+let containerNewMensageRecive = document.querySelector('.chat-window__recive');
 let chatWindow = document.querySelector('.chat__window');
 
-const socket = new WebSocket("ws://websocket-java-production.up.railway.app/ws");
+const socket = new WebSocket("ws://localhost:8080/ws");
+
 
 socket.onmessage = function (event) {
-    let mensage = containerNewMensage.cloneNode(true);
-   
+    let mensageGive = containerNewMensageGive.cloneNode(true);
+    let mensageRecive = containerNewMensageRecive.cloneNode(true);
+
     var dataAtual = new Date();
     var horaAtual = dataAtual.toLocaleTimeString();
-    
-    mensage.querySelector('.chat-window__give-msg').innerHTML = '';
-    mensage.querySelector('.chat-window__give-msg').innerHTML = event.data;
 
-    mensage.querySelector('.chat-window__give-time').innerHTML = '';
-    mensage.querySelector('.chat-window__give-time').innerHTML = horaAtual;
+    let retornoMensagem = event.data.split(":");
+    console.log(retornoMensagem)
 
-    chatWindow.appendChild(mensage);
+    if(retornoMensagem[0] === "Enviada"){
 
+    mensageGive.querySelector('.chat-window__give-msg').innerHTML = '';
+    mensageGive.querySelector('.chat-window__give-msg').innerHTML = retornoMensagem[1];
+
+    mensageGive.querySelector('.chat-window__give-time').innerHTML = '';
+    mensageGive.querySelector('.chat-window__give-time').innerHTML = horaAtual;
+    chatWindow.appendChild(mensageGive);
     let newMensageSound = document.querySelector('.newMensage__sound');
     newMensageSound.play();
-
     inputMensage.value = '';
+
+    }else {
+        
+        mensageRecive.querySelector('.chat-window__recive-msg').innerHTML = '';
+        mensageRecive.querySelector('.chat-window__recive-msg').innerHTML = retornoMensagem;
+    
+        mensageRecive.querySelector('.chat-window__recive-time').innerHTML = '';
+        mensageRecive.querySelector('.chat-window__recive-time').innerHTML = horaAtual;
+        chatWindow.appendChild(mensageRecive);
+
+    }
+   
+   
+    
 
 }
 
